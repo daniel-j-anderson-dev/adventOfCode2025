637b176252eb3c4404dfc8a60e6d196fbf4d1c3f
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-12-05 12:58:44 -0700

    changed import renames

--- src/AdventOfCode.hs
+++ src/AdventOfCode.hs
@@ -2,16 +2,16 @@
 
 module AdventOfCode where
 
-import qualified Data.Text.IO as T
 import qualified Data.Text as T
-import qualified Data.Text.Read as T
+import qualified Data.Text.IO as T.IO
+import qualified Data.Text.Read as T.R
 
 
 inputPath :: Word -> FilePath
 inputPath n = "./input/day" ++ show n ++ ".txt" 
 
 input :: Word -> IO T.Text
-input n = T.readFile (inputPath n)
+input n = T.IO.readFile (inputPath n)
 
 data Puzzle = Part1 | Part2
 
@@ -19,6 +19,6 @@ eitherToMaybe :: Either err ok -> Maybe ok
 eitherToMaybe = either (const Nothing) Just
 
 parseIntegralBase10 :: Integral a => T.Text -> Maybe a
-parseIntegralBase10 s = case T.decimal s of
+parseIntegralBase10 s = case T.R.decimal s of
   Right (n, unparsed) | T.null unparsed -> Just n
   _ -> Nothing
