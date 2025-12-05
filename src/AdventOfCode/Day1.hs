0dd3399823ffc8365850ce36c760cca0a48c2c94
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-12-05 12:58:58 -0700

    used Data.Text

--- src/AdventOfCode/Day1.hs
+++ src/AdventOfCode/Day1.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module AdventOfCode.Day1 where
 
-import AdventOfCode (Puzzle (Part1, Part2))
+import AdventOfCode (Puzzle (Part1, Part2), parseIntegralBase10)
 import Data.Maybe (mapMaybe)
 import Text.Read (readMaybe)
+import qualified Data.Text as T
 
 dialStart :: Int
 dialStart = 50
@@ -10,17 +13,17 @@ dialStart = 50
 dialSize :: Int
 dialSize = 100
 
-rotationFromString :: String -> Maybe Int
-rotationFromString s = do
-  direction <- case s of
-    ('L' : _) -> Just (-1)
-    ('R' : _) -> Just 1
+parseLine :: T.Text -> Maybe Int
+parseLine s = do
+  (direction, n) <- case T.uncons s of
+    Just ('L', n) -> Just (-1, n)
+    Just ('R', n) -> Just (1, n)
     _ -> Nothing
-  distance <- readMaybe (drop 1 s)
+  distance <- parseIntegralBase10 n
   Just (direction * distance)
 
-parseInput :: String -> [Int]
-parseInput input = mapMaybe rotationFromString (lines input)
+parseInput :: T.Text -> [Int]
+parseInput input = mapMaybe parseLine (T.lines input)
 
 zeroPasses :: Int -> Int -> Int
 zeroPasses dialPosition offset
@@ -32,7 +35,7 @@ zeroPasses dialPosition offset
 applyRotation :: Int -> Int -> Int
 applyRotation dialPosition offset = (dialPosition + offset) `mod` dialSize
 
-solution :: Puzzle -> String -> Int
+solution :: Puzzle -> T.Text -> Int
 solution part input = fst (foldl step (0, dialStart) (parseInput input))
   where
     step (zeroCount, dialPosition) offset =
