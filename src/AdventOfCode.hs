098e2c6795d1525f184b45c432fcdc4ca698643a
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-12-05 12:53:42 -0700

    use Data.Text

--- src/AdventOfCode.hs
+++ src/AdventOfCode.hs
@@ -1,9 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module AdventOfCode where
 
-import Data.Maybe (mapMaybe)
-import Text.Read (readMaybe)
+import qualified Data.Text.IO as T
+import qualified Data.Text as T
+import qualified Data.Text.Read as T
 
-input :: Word -> IO String
-input n = readFile ("./input/day" ++ show n ++ ".txt")
+
+inputPath :: Word -> FilePath
+inputPath n = "./input/day" ++ show n ++ ".txt" 
+
+input :: Word -> IO T.Text
+input n = T.readFile (inputPath n)
 
 data Puzzle = Part1 | Part2
+
+eitherToMaybe :: Either err ok -> Maybe ok
+eitherToMaybe = either (const Nothing) Just
+
+parseIntegralBase10 :: Integral a => T.Text -> Maybe a
+parseIntegralBase10 s = case T.decimal s of
+  Right (n, unparsed) | T.null unparsed -> Just n
+  _ -> Nothing
